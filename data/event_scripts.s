4815f7f7d6c5649e2d19b585fc8132fc5db62c2f
wooper125 <215932917+wooper125@users.noreply.github.com>
2025-06-17 20:59:55 -0400

    fixed some region map stuff

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1157,3 +1157,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/dexnav.inc"
+
+	.include "data/maps/Route1/scripts.inc"
